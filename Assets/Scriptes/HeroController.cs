7c16a169f9f815351e5ad551a46188690ea1e4cf
logan <logan.villaret@gmail.com>
2026-03-02 00:29:03 +0100

    1213

--- Assets/Scriptes/HeroController.cs
+++ Assets/Scriptes/HeroController.cs
@@ -1,40 +1,40 @@
-using System.Xml.Schema;
-using UnityEngine;
-using UnityEngine.InputSystem;
+//using System.Xml.Schema;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
 
-public class HeroController : MonoBehaviour
-{
-    private Rigidbody2D rb;
+//public class HeroController : MonoBehaviour
+//{
+    //private Rigidbody2D rb;
     
-    private float horizontalValue;
+   // private float horizontalValue;
 
-    [SerializeField] public float speed = 1;
+   // [SerializeField] public float speed = 1;
 
-    void Awake()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
+  //  void Awake()
+   // {
+ //       _rb = GetComponent<Rigidbody2D>();
+  //  }
 
-    void Update()
-    {
-        horizontalValue = Input.GetAxisRaw("Horizontal");
-    }
+//    void Update()
+  //  {
+      //  horizontalValue = Input.GetAxisRaw("Horizontal");
+   // }
 
-    private void FixedUpdate()
-    {
-        Move(horizontalValue);
-    }
+  //  private void FixedUpdate()
+    //{
+      //  Move(horizontalValue);
+  //  }
 
-    private void Move(float dir)
-    {
-        float xVal = horizontalValue * speed * Time.deltaTime;
+  //  private void Move(float dir)
+   // {
+    //    float xVal = horizontalValue * speed * Time.deltaTime;
 
-    private Vector2 targetVelocity = new Vector2(xVal, rb.velocity.y);
+  //  private Vector2 targetVelocity = new Vector2(xVal, rb.velocity.y);
     
-    rb.velocity = targetVelocity;
-}
+    //rb.velocity = targetVelocity;
+//}
     
     
     
 
-}
+
